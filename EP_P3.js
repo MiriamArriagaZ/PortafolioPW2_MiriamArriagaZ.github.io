0463f6af2cd71f602262b21053e944739028ef4e
MiriamArriagaZ <125667010+MiriamArriagaZ@users.noreply.github.com>
2023-05-19 07:54:16 -0600

    Add files via upload
    Signed-off-by: MiriamArriagaZ <125667010+MiriamArriagaZ@users.noreply.github.com>

---
+++ EP_P3.js
@@ -0,0 +1,8 @@
+function ObtenerDescuento()
+{
+    var fecha=document.getElementById("fecha").value;
+    if(fecha==)
+    {
+        document.getElementById("resul").innerHTML="Hoy es tu cumpleaños";
+    }
+}
